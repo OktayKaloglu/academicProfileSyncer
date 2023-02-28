8fa203a0239d0777b10dba09cc987c5852c54739
oktay kaloglu <kaloglu.oktay@gmail.com>
2023-02-28 22:02:48 +0300

    there is problem with deu, the rest of the codde for parsing is implemented. needs to csv conversion and parallelization

--- uniParser/components/pageParser.py
+++ uniParser/components/pageParser.py
@@ -1,37 +1,65 @@
-from urllib.request import urlopen
+from urllib.request import urlopen,Request
 from bs4 import BeautifulSoup
+import datetime
 
-def getInstructorName(url:str,path:str)->str:
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+
+def lessonParser(url:str,instructorPath:str,lessonPath:str)->list:
     
-    page=urlopen(url)
-    soup=BeautifulSoup(page.read())
-    name = soup.select(path)
-    #todo soup returns html element .text is not working to get the string find a way
-    return name
-
+    
+    page=urlopen(Request(url=url,headers=headers))
+    print("status: ",page.getcode())
+    
+    if page.getcode() ==200:
+        soup=BeautifulSoup(page.read(),features="html.parser")
+        #select returns an array hence we give the direct path to element we just need the first element.
+        instructors= soup.select(instructorPath)
+        instructorNames=[]
+        if len(instructors)>0 :
+            for instructor in instructors:
+                instructorNames.append(instructor.get_text(strip=True))
+        
+        lesson=soup.select(lessonPath)
+        lessonName=""
+        if len(lesson)>0:
+            lessonName=lesson[0].get_text(strip=True)
+        
+        arr=[instructorNames,lessonName,url]
+        print(arr)
+        return arr
+    
+    return []
 
 
 def parsePage(university:dict)->list:
-    if university["initials"]=="iyte" :    
+    
+    data = []
+        
+    if university["initials"]=="deu" :    
+        
+        
         url=university["url_endpoint"]
-        page=urlopen(url)
         
+        print("university: ",university["name"]," status: ",end="")
+        page=urlopen(Request(url=url,headers=headers))
+        print(page.getcode())
         
-        soup=BeautifulSoup(page.read(),features="html.parser")
-        
-        data = []
-        table = soup.find('table')
-        table_body = table.find('tbody')
+        if page.getcode()==200:
+            soup=BeautifulSoup(page.read(),features="html.parser")
+            
+            rows=soup.select(university["course_href_path"])
+            for row in rows:
+                href=row.get("href")
+                if university["university_href_includes_domain"] :  
+                    url= href 
+                else: 
+                    url= university["university_prefix"]+href
+                print(datetime.datetime.now(),"  Parsing: ",url,end=" ")
+                data.append(lessonParser(url,university["teacher_path"],university["lesson_name_path"]))    
 
-        rows = table_body.find_all('tr')
-        for row in rows:
-            cols = row.find_all('td')
-            cols = [ele.text.strip() for ele in cols]
-            data.append([ele for ele in cols if ele]) # Get rid of empty values
-                
         
         
-        #getInstructorName()
         
     
-    return []
+    return data
