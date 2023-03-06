a3f86c195819e845385113ec512d4b4fdf05cb99
oktay kaloglu <kaloglu.oktay@gmail.com>
2023-03-06 19:07:32 +0300

    bug fix for yasar

--- uniParser/main.py
+++ uniParser/main.py
@@ -6,38 +6,44 @@ from components.pageParser import parsePage
 import csv
 
 
-def writeCsv(arr,file):
+def writeCsv(parsedData,file):
     with open(file, 'w', newline='') as csv_file:
      writer = csv.writer(csv_file)
      for uni in parsedData:
         #an error occurs by the ege uni.
         # teachers line has /n init 
         for row in uni:
-            if "\n" in row[0]:
-                row[0]= row[0].split("\n")
+            
             for teacher in row[0]:
                 writer.writerow([teacher,row[1],row[2]])
+            
+            
     csv_file.close()
     
 
-
-# get all the universities from the universities file
-universities=readJSON('universities.json')
-
-
-
-parsedData=[]
-# Iterating through the Universities
-time=datetime.now()
-
-for university in universities['universities']:
-    parsedData.append(parsePage(university))
+def parseLessons():
+    # get all the universities from the universities file
+    universities=readJSON('universities.json')
+    
 
 
 
-sleep(27)
-#print(parsedData)
+    parsedData=[]
+    # Iterating through the Universities
+    time=datetime.now()
 
-writeCsv(parsedData,"teachers.csv")
-time2=datetime.now()
-print("time takes: ",time2-time)
+    for university in universities['universities']:
+        parsedData.append(parsePage(university))
+
+
+
+    sleep(27)
+    #print(parsedData)
+
+    writeCsv(parsedData,"teachers.csv")
+    time2=datetime.now()
+    print("time takes: ",time2-time)
+
+
+if __name__ == '__main__':
+    parseLessons()
