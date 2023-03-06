6bc5d1925b0cddc3ca20e257cb1ade9c291e838d
oktay kaloglu <kaloglu.oktay@gmail.com>
2023-03-06 16:32:18 +0300

    more than 1 table error is resolved,csv write added,ege uni adjusted for multi threading.

--- uniParser/components/pageParser.py
+++ uniParser/components/pageParser.py
@@ -6,32 +6,39 @@ import datetime
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
-def lessonParser(url:str,instructorPath:str,lessonPath:str)->list:
-    
-    
-    page=urlopen(Request(url=url,headers=headers))
-    
-    
-    if page.getcode() ==200:
-        soup=BeautifulSoup(page.read(),features="html.parser") 
-        # todo
-        # table:nth-child(12) like css selectors not working with the soup select
-        # find a way to trick that
-        instructors= soup.select(instructorPath)
-        instructorNames=[]
-        if len(instructors)>0 :
-            for instructor in instructors:
-                instructorNames.append(instructor.get_text(strip=True))
+def lessonParser(url:str,instructorPath:str,lessonPath:str,instructorTableIdx:int)->list:
+    for i in range(3):
+        try:
+           
+            page=urlopen(Request(url=url,headers=headers),timeout=10)
+        except:
+            continue
+        if page.getcode() ==200:
+            soup=BeautifulSoup(page.read(),features="html.parser") 
+            
+            # table:nth-child(12) like css selectors not working with the soup select
+            # the solution is give the table index and then select the element
+            if instructorTableIdx==0:
+                instructors= soup.select(instructorPath)
+            else:
+                
+                instructors=soup.select("table")[instructorTableIdx]
+                instructors=instructors.select(instructorPath)
+
+            instructorNames=[]
+            if len(instructors)>0 :
+                for instructor in instructors:
+                    instructorNames.append(instructor.get_text(strip=True))
+            
+            lesson=soup.select(lessonPath)
+            lessonName=""
+            if len(lesson)>0:
+                lessonName=lesson[0].get_text(strip=True)
+            
+            arr=[instructorNames,lessonName,url]
+            print(datetime.datetime.now()," status: ",page.getcode(),", ",arr)
+            return arr
         
-        lesson=soup.select(lessonPath)
-        lessonName=""
-        if len(lesson)>0:
-            lessonName=lesson[0].get_text(strip=True)
-        
-        arr=[instructorNames,lessonName,url]
-        print(datetime.datetime.now()," status: ",page.getcode(),", ",arr)
-        return arr
-    
     return []
 
 
@@ -39,7 +46,7 @@ def parsePage(university:dict)->list:
     
     data = []
         
-    if university["initials"]!="ege" :    
+    if university["initials"]!="0ege" :    
         
         
         url=university["url_endpoint"]
@@ -61,7 +68,7 @@ def parsePage(university:dict)->list:
                     else: 
                         url= university["university_prefix"]+href
                     
-                    result=pool.submit(lessonParser,url,university["teacher_path"],university["lesson_name_path"])
+                    result=pool.submit(lessonParser,url,university["teacher_path"],university["lesson_name_path"],university["teacher_table_idx"])
                     results.append(result)
                 wait(results)
             for res in results:
--- uniParser/main.py
+++ uniParser/main.py
@@ -9,23 +9,17 @@ import csv
 def writeCsv(arr,file):
     with open(file, 'w', newline='') as csv_file:
      writer = csv.writer(csv_file)
-     for row in arr:
-         writer.writerow(row)
+     for uni in parsedData:
+        #an error occurs by the ege uni.
+        # teachers line has /n init 
+        for row in uni:
+            if "\n" in row[0]:
+                row[0]= row[0].split("\n")
+            for teacher in row[0]:
+                writer.writerow([teacher,row[1],row[2]])
     csv_file.close()
     
-def readCsv(path):
-    with open(path) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                line_count += 1
-        print(f'Processed {line_count} lines.')
-    csv_file.close()
+
 
 # get all the universities from the universities file
 universities=readJSON('universities.json')
@@ -40,7 +34,10 @@ for university in universities['universities']:
     parsedData.append(parsePage(university))
 
 
-time2=datetime.now()
+
 sleep(27)
-print(parsedData)
+#print(parsedData)
+
+writeCsv(parsedData,"teachers.csv")
+time2=datetime.now()
 print("time takes: ",time2-time)
