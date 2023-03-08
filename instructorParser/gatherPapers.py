5777a70a3651e92eebdc5c428d74fa6772715c26
oktay kaloglu <kaloglu.oktay@gmail.com>
2023-03-08 05:53:10 +0300

    paper gatherer implemented. needs improvement.

--- instructorParser/gatherPapers.py
+++ instructorParser/gatherPapers.py
@@ -1,5 +1,6 @@
 from scholarly import ProxyGenerator,scholarly
 from jsonWrite import writeJson
+from concurrent.futures import ThreadPoolExecutor,wait
 # Set up a ProxyGenerator object to use free proxies
 # This needs to be done only once per session
 pg = ProxyGenerator()
@@ -10,11 +11,22 @@ scholarly.use_proxy(pg)
 # Retrieve the author's data, fill-in, and print
 search_query = scholarly.search_author('Murat Osman ÜNALIR')
 author = scholarly.fill(next(search_query))
-writeJson( author,"author.json")
 
-# Take a closer look at the first publication
-pub = scholarly.fill(author['publications'][0])
-print(pub)
+pubs=[]
+
+with ThreadPoolExecutor(max_workers=1000) as pool:
+    results=[]
+    for pub in author['publications']:
+        #pubs.append(scholarly.fill(pub))
+        print(pub["bib"]["title"])
+        result=pool.submit(scholarly.fill,pub)
+        results.append(result)
+    wait(results)
+    for res in results:
+        pubs.append(res)
+
+
+writeJson(pubs,"MuratOsmanÜnalir.json")
 
 """
 # Print the titles of the author's publications
