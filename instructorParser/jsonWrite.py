09d530d740937d0cbb8a6b4b21154636e1a6bd66
okanwyl <ok4nakyol@gmail.com>
2023-04-12 21:01:46 +0300

    feat: base model init

--- instructorParser/jsonWrite.py
+++
@@ -1,6 +0,0 @@
-import json
-
-def writeJson(arr:list,path:str)->None:
-    json_object = json.dumps(arr,ensure_ascii=False, indent=4)
-    with open(path, "w",encoding="UTF-8") as outfile:
-        outfile.write(json_object)
